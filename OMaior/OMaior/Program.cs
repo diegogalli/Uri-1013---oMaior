ceaf42a212680f2f57cdc2cdf90eaef265e10398
diegogalli <84423207+diegogalli@users.noreply.github.com>
2021-08-11 14:13:14 -0300

    Uri 1013 o maior

---
+++ OMaior/OMaior/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+namespace OMaior
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A, B, C, MAIORAB, MAIOR;
+
+            string[] valores = Console.ReadLine().Split(' ');
+            A = int.Parse(valores[0]);
+            B = int.Parse(valores[1]);
+            C = int.Parse(valores[2]);
+
+            MAIORAB = (A + B + Math.Abs(A - B))/ 2;
+            MAIOR = (MAIORAB + C + Math.Abs (MAIORAB - C))/ 2;
+
+            Console.WriteLine(MAIOR + " eh o maior");
+            Console.ReadLine();
+        }
+    }
+}
